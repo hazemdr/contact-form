c6ef5b42208a9f95060aa1e2b889118be7b91168
hazem <livehazem@gmail.com>
2024-10-28 14:48:39 +0100

    first commit about this form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule,FormGroup,FormControl,FormBuilder } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'contactForm';
+  contactForm:FormGroup
+  constructor(private fb:FormBuilder){
+    let controls = {
+      name: new FormControl('',[] ),
+      email: new FormControl('',[] ),
+      subject: new FormControl('',[] ),
+      message: new FormControl('',[] )
+    }
+    this.contactForm = this.fb.group(controls)
+  }
+  send(){
+    console.log(this.contactForm.value);
+    
+    
+  }
 }
